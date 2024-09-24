42f38e2d2673f01316dd64062ff130a6f6130e8c
averbel1 <averbel1@cuc-edu.co>
2024-09-24 13:05:48 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import miLogo from "../src/assets/img/images.jpeg"
+function App() {
+  
+  return (
+    <>
+      <div>
+        <img src={miLogo} alt="junior" />
+        <h1>taller03</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
